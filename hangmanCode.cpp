bfc8bf03622862115aa19461d23a2d6b467d4a97
Nguyễn Thị Trà My <myn897676@gmail.com>
2024-08-08 17:13:59 +0700

    Add files via upload

---
+++ hangmanCode.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int handleRandom(string mode, vector<int> &check)
+{
+    // return số dòng
+}
+void printABC()
+{
+}
+
+void showHiddenWord() {}
+
+void showHangMan() {}
+
+void showWinLoose()
+{
+    // loose xong chuyển qua nhập tên
+}
+
+void showList() {}
+
+void showInit() {}
+
+void showUI()
+{
+}
+
+void startGame() {}
+
+int main()
+{
+    startGame();
+    return 0;
+}
